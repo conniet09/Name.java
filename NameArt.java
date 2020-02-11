b074c5eddcaccd42da5b61ede4991cb80ef76d99
conniet09 <59925548+conniet09@users.noreply.github.com>
2020-02-10 20:57:23 -0500

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,59 @@
+/*********************************
+*NameArt.java
+**********************************
+*Name in Code
+*This project displays my name in Code
+*Connie Truong
+*January 23, 2020
+*CMSC 255 Section 3
+**********************************/
+
+public class NameArt {
+
+	public static void main(String[] args) {
+
+		System.out.println("Connie Truong");
+		System.out.println("Project 1 - Name in Code");
+		System.out.println("This project displays my name in Code");
+		System.out.println("January 23, 2020");
+		System.out.println("CMSC 255 Section 3");
+		System.out.println("*************************************************************************************");
+		System.out.println("-------------------------------------------------------------------------------------");
+		System.out.println("|*|                               ^             ^                                 |*|");
+		System.out.println("|*|                              | |           | |                                |*|");
+		System.out.println("|*|                              | |           | |                                |*|");
+		System.out.println("|*|                              | |           | |                                |*|");
+		System.out.println("|*|                                -----------                                    |*|");
+		System.out.println("|*|                            |                  |                               |*|");
+		System.out.println("|*|                        =====     *      *     =====                           |*|");
+		System.out.println("|*|                        =====         v        =====                           |*|");
+		System.out.println("|*|    *---------oo0----------------------------------------------------------*   |*|");
+		System.out.println("|*|    |                                                                      |   |*|");
+		System.out.println("|*|    |                                                                      |   |*|");
+		System.out.println("|*|    |      *****     ****     * *      *     * *      *    *     *****     |   |*|");
+		System.out.println("|*|    |     *     *   *    *    *  *     *     *  *     *    *     *         |   |*|");
+		System.out.println("|*|    |     *         *    *    *   *    *     *    *   *    *     *         |   |*|");
+		System.out.println("|*|    |     *         *    *    *    *   *     *     *  *    *     *****     |   |*|");
+		System.out.println("|*|    |     *         *    *    *     *  *     *      * *    *     *         |   |*|");
+		System.out.println("|*|    |     *     *   *    *    *      * *     *       **    *     *         |   |*|");
+		System.out.println("|*|    |      *****     ****     *        *     *        *    *     *****     |   |*|");
+		System.out.println("|*|    |                                                                      |   |*|");
+		System.out.println("|*|    |                                                                      |   |*|");
+		System.out.println("|*|    *-----------------------------------------------------0oo--------------*   |*|");
+		System.out.println("|*|                           |       |      |       |                            |*|");
+		System.out.println("|*|                           |       |      |       |                            |*|");
+		System.out.println("|*|                           |       |      |       |                            |*|");
+		System.out.println("|*|                           |       |      |       |                            |*|");
+		System.out.println("|*|                           |       |      |       |                            |*|");
+		System.out.println("|*|                           |       |      |       |                            |*|");
+		System.out.println("|*|                           ---------      ---------                            |*|");
+		System.out.println("|*|                          |         |    |         |                           |*|");
+		System.out.println("|*|                          |         |    |         |                           |*|");
+		System.out.println("|*|                          |         |    |         |                           |*|");
+		System.out.println("|*|                        ---         |    |          ---                        |*|");
+		System.out.println("|*|                       (            |    |             )                       |*|");
+		System.out.println("|*|                        *___________      ____________*                        |*|");
+		System.out.println("-------------------------------------------------------------------------------------");
+		System.out.println("*************************************************************************************");
+		}
+}
